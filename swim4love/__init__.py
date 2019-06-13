6367d499a0da43927969c79767d4d0d4575c376f
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-06-13 20:37:21 +0800

    Commented out secrets.py ("# ")

--- swim4love/__init__.py
+++ swim4love/__init__.py
@@ -13,7 +13,7 @@ SERVER_MODE = 'development'
 ##################### App Initialization #####################
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(app_config[SERVER_MODE])
-app.config.from_pyfile('secrets.py')
+# app.config.from_pyfile('secrets.py')
 
 db = SQLAlchemy()
 db.app = app  # Fix for weird "No application found" error
