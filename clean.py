6841200b0dd19cfd75c064ac3b5c57e2c1bad80b
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-06-14 23:25:03 +0800

    Implement registration

--- clean.py
+++ clean.py
@@ -4,6 +4,7 @@ from config import Config
 from swim4love.site_config import ROOT_DIR, AVATAR_DIR, DEFAULT_AVATAR
 
 
+print('make sure the server is shut down before cleaning')
 for avatar_filename in os.listdir(os.path.join(ROOT_DIR, AVATAR_DIR)):
     if avatar_filename != DEFAULT_AVATAR:
         os.remove(os.path.join(ROOT_DIR, AVATAR_DIR, avatar_filename))
