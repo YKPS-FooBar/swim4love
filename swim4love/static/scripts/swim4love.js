78b9d16a5c98c3126c81f0b71a5353cb9fd55940
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-06-14 16:53:13 +0800

    Fix bug of leaderboard

--- swim4love/static/scripts/swim4love.js
+++ swim4love/static/scripts/swim4love.js
@@ -2,11 +2,9 @@ let socket; // for websocket receiving data
 let leaderboard; // for actual displaying
 let topTen; // for updating
 let previousData = {}; // for checking changes
-let timestamp = {}; // for sorting by order of finish
-let totalLaps = 0; // for tallying
+// A mapping from `id` to a Date.now() that records the time of last change
+let timestamp = {};
 
-let tallyLap;
-let tallyMeter;
 
 $(document).ready(() => {
     if (!($(document).width() === 1920 && $(document).height() === 1080)) console.warn('Your screen dimensions might not be optimized to display this page. Consider changing to a 1920×1080 size.');
@@ -15,24 +13,38 @@ $(document).ready(() => {
     socket = io.connect(window.location.origin);
     socket.on('init', raw => process_data(raw, () => topTen.forEach((e, i) => insert_player(e, null, i))));
     socket.on('swimmers', process_data);
-    tallyLap = $('.tally.laps.num');
-    tallyMeter = $('.tally.meters.num');
     leaderboard = [];
 });
 
+
 function process_data(raw, callback=update_leaderboard) {
-    let keys = Object.keys(raw);
-    let values = Object.values(raw);
-    let data = keys.map((e, i) => Object({id: e, name: values[i]['name'], laps: values[i]['laps']}));
+    // `raw` is {id: {id: 123, name: 'Name', laps: 10}, ...}
+    // `data` is [{id: '123', name: 'Name', laps: 10}, ...]
+    let data = Object.values(raw).map(swimmer => Object({
+        id: swimmer.id.toString(),
+        name: swimmer.name,
+        laps: swimmer.laps
+    }));
+
     data.forEach((e, i) => {
         let id = e.id
-        if (timestamp[id] === undefined || (previousData[id] !== undefined && e.laps - previousData[id] !== 0)) {
+        // If the `laps` is updated or `timestamp[id]` is yet to be created,
+        // set value for timestamp[id]
+        if (timestamp[id] === undefined || (previousData[id] !== undefined && e.laps !== previousData[id])) {
             timestamp[id] = Date.now() + i;
         }
     });
+    // Sort `data` according to laps -> timestamp
     data.sort((a, b) => a.laps > b.laps ? -1 : (a.laps < b.laps ? 1 : timestamp[a.id] < timestamp[b.id] ? -1 : 1));
-    let newTotal = data.map(e => e.laps).reduce((a, b) => a + b);
+
+    let newTotal;
+    if (data.length === 0) {
+        newTotal = 0;
+    } else {
+        newTotal = data.map(e => e.laps).reduce((a, b) => a + b);
+    }
     tally(newTotal);
+
     topTen = data.slice(0, 10);
     leaderboard = topTen;
     callback();
@@ -41,13 +53,14 @@ function process_data(raw, callback=update_leaderboard) {
     });
 }
 
+
 function update_leaderboard() {
-    let leaderboardIds = leaderboard.map(e => e.id);
-    let topTenIds = topTen.map(e => e.id);
     if (leaderboard.length > topTen.length) {
         console.warn('Suspicious decreased leaderboard players!');
         return;
     }
+    let leaderboardIds = leaderboard.map(e => e.id);
+    let topTenIds = topTen.map(e => e.id);
     leaderboard.forEach(e => {
         let id = e.id;
         if (!topTenIds.includes(id)) remove_player(id); // fell out of leaderboard
@@ -59,32 +72,35 @@ function update_leaderboard() {
             insert_player(e, null, i, false);
             return;
         }
-        // let diffLaps = laps - parseInt($(`#${id} .laps`).text());
-        let diffLaps = laps - previousData[id];
-        if (diffLaps !== 0) {
-            increment($(`#${id} .laps`), diffLaps);
-            increment($(`#${id} .meters`), diffLaps * LAP_LENGTH);
+
+        if (laps !== previousData[id]) {
+            change_number($(`#${id} .laps`), laps);
+            change_number($(`#${id} .meters`), laps * LAP_LENGTH);
         }
         change_rank(id, i);
     });
 }
 
+
 function tally(newTotal) {
-    // let tallyDiff = newTotal - totalLaps;
-    let tallyDiff = newTotal;
-    if (Object.keys(previousData).length !== 0) tallyDiff -= Object.values(previousData).reduce((a, b) => a + b);
-    if (tallyDiff !== 0) {
-        increment(tallyLap, tallyDiff);
-        increment(tallyMeter, tallyDiff * LAP_LENGTH);
-        totalLaps = newTotal;
-    }
+    change_number($('.tally.laps.num'), newTotal);
+    change_number($('.tally.meters.num'), newTotal * LAP_LENGTH);
 }
 
-function increment(element, updateCount) {
-    let current = parseInt(element.text());
-    element.prop('counter', current).animate({counter: current + updateCount}, {
-        duration: METER_UPDATE_SPAN,
-        easing: 'swing',
-        step: now => element.text(Math.ceil(now))
-    });
+
+function change_number(element, updated) {
+    element.prop('counter', parseInt(element.text())).animate(
+        {
+            // a dummy counter
+            counter: updated
+        },
+        {
+            // how long the animation would be
+            duration: METER_UPDATE_SPAN,
+            // which easing function to use
+            easing: 'swing',
+            // update text each step
+            step: now => element.text(Math.ceil(now))
+        }
+    );
 }
