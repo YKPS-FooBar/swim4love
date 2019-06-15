59696e53d1f41f7f850f13aa4834daff69b37aeb
HanwenZhu <thomas.zhu.sh@hotmail.com>
2019-06-15 08:29:44 +0800

    Change to swim4love.com

--- swim4love/site_config.py
+++ swim4love/site_config.py
@@ -1,6 +1,5 @@
 # Root URL
-print('change localhost to swim4love.com in production environment')
-ROOT_URL = 'http://localhost:5000'
+ROOT_URL = 'http://swim4love.com'
 
 # Length of digits in swimmer's ID
 SWIMMER_ID_LENGTH = 3
